7787525f025c14574260e2467ef9ecf9e26503e5
Joakim Nyman <joakim.nyman@vidhance.com>
2021-01-14 14:55:45 +0100

    printing vars

--- groovyTest/helper.groovy
+++ groovyTest/helper.groovy
@@ -3,7 +3,9 @@ import jenkins.model.Jenkins
 
 def test1() {
     sh(script: 'echo test1', label: 'test1')
-    sh(script: 'echo variable', label: "Branch: ${env.BRANCH}")
+    sh(script: 'echo variable', label: "Branch: ${env.BRANCH_NAME}")
+    sh "echo ${global_var}"
+    sh "echo ${private_var}"
 }
 
 return this
